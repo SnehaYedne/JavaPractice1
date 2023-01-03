c784720014a5d9a4f9239a2c33c2d4c4434043cc
SnehaYedne <121806899+SnehaYedne@users.noreply.github.com>
2023-01-03 13:36:43 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Array {
+    public static void main(String[] args) {
+       int age[]= {5,10,4,6,7};
+        System.out.println("first element: "+age[0]);;
+        System.out.println("second element :"+age[1]);
+        System.out.println("third element: "+age[2]);
+    }
+}
